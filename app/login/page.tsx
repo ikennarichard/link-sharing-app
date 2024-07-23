30385230a1367df2ce3c92ad800b289ebec94d62
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-23 16:45:37 +0100

    chore: modify page title

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,17 @@
+import Link from "next/link";
 import Input from "../components/input";
 import LogoText from "../components/logo-text";
 import SubmitButton from "../components/submit-button";
 import Envelope from "../ui/icons/envelope";
 import Lock from "../ui/icons/lock-key";
 
-export default function Home() {
+export default function LoginPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-[3.19rem] overflow-hidden">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-[3.19rem] overflow-hidden py-2">
       <LogoText />
       <form
         action=""
-        className="flex flex-col gap-10 max-w-[29.1rem] rounded-lg bg-white p-10"
+        className="flex flex-col gap-10 w-[29.8rem] rounded-lg bg-white p-10"
       >
         <div>
           <h2 className="text-dark-grey text-[2rem] font-bold leading-[3rem]">
@@ -27,19 +28,21 @@ export default function Home() {
             label="Email address"
             placeholder="e.g. alex@email.com"
             icon={<Envelope />}
-            name="user_password"
+            name="user_mail"
           />
           <Input
             type="password"
             label="Password"
             placeholder="Enter your password"
             icon={<Lock />}
-            name="user_mail"
+            name="user_password"
           />
           <SubmitButton label="Login" />
           <p className="text-nickel text-[1rem] text-center leading-[1.5rem]">
             Don’t have an account?{" "}
-            <span className="text-han-purple text-[1rem]">Create account</span>
+            <Link href="/signup" className="text-han-purple text-[1rem]">
+              Create account
+            </Link>
           </p>
         </div>
       </form>
