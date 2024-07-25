9b429c3b1773e3e2328d6bb4baca76475159a780
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-25 10:59:18 +0100

    feat: check for logged in user

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+import { createClient } from "@/utils/supabase/server";
+import CustomizeLinks from "./components/customize-links/customize-links";
+import Header from "./components/header";
+import MobileIllustration from "./components/mobile-illustration/mobile-illustration";
+import Profile from "./components/profile-details/profile";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  if (error || !user) {
+    redirect("/login");
+  }
+
+  let metadata = user.user_metadata;
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      
+    <main className="flex flex-col min-h-screen px-6 py-6">
+      <Header />
+      <section className="grid grid-cols-2 gap-2">
+        <MobileIllustration />
+        {user ? <CustomizeLinks /> : <Profile />}
+      </section>
     </main>
   );
 }
