0bfbd34b0b10a23a5d61564d536013a12790a55a
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-25 10:56:26 +0100

    feat: check form status when submitting

--- app/components/submit-button.tsx
+++ app/components/submit-button.tsx
@@ -1,10 +1,15 @@
+'use client'
+
+import { useFormStatus } from "react-dom";
+
 export default function SubmitButton({ label }: { label: string }) {
+  const { pending } = useFormStatus();
   return (
     <button
       type="submit"
-      className="text-[1rem] leading-6 px-[1.69rem] py-[0.68rem] text-white bg-han-purple text-center rounded-lg font-semibold"
+      className="text-[1rem] leading-6 px-[1.69rem] py-[0.68rem] text-white bg-han-purple text-center rounded-lg font-semibold hover:bg-hover-purple hover:shadow-light transition-all duration-200 ease-in-out border-none"
     >
-      {label}
+      {pending ? 'Loading' : label}
     </button>
   );
 }
