5f859dd19f3bb4beb3783ffbea6534bdf40a5529
Ikenna Richard <oguejioforrichard@gmail.com>
2024-07-25 11:00:15 +0100

    chore: modify input component

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,33 +1,30 @@
 import { ReactNode } from "react";
 
 type InputProperties = {
-  name: string;
-  type: string;
   label: string;
+  labelFor: string;
   icon: ReactNode;
-  placeholder: string;
+  children: ReactNode
 };
 
 export default function Input({
-  name,
-  type,
   label,
+  labelFor,
   icon,
-  placeholder,
+  children
 }: InputProperties) {
   return (
-    <p>
-      <label htmlFor={name} className="text-[0.75rem] leading-[1.125rem] text-dark-grey">{label}</label>
-      <p className="flex gap-3 items-center py-3 px-4 rounded-lg border-[1px] border-light-silver">
-        {icon}
-        <input
-          className="text-[1rem] text-dark-grey leading-[1.5rem]"
-          type={type}
-          name={name}
-          id={name}
-          placeholder={placeholder}
-        />
-      </p>
-    </p>
+    <div>
+      <label
+        htmlFor={labelFor}
+        className="text-[0.75rem] leading-[1.125rem] text-dark-grey"
+      >
+        {label}
+      </label>
+      <div className="flex gap-3 items-center relative">
+        <span className="absolute left-4">{icon}</span>
+        {children}
+      </div>
+    </div>
   );
 }
